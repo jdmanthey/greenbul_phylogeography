f0abbbf112e21809e5f6da2e1ed2684c6cd10131
Joe Manthey <jdmanthey@gmail.com>
2025-06-02 11:19:44 -0500

    Update 05_filter_redo.sh

--- 05_filter_redo.sh
+++ 05_filter_redo.sh
@@ -16,8 +16,8 @@ workdir=/lustre/scratch/jmanthey/greenbul_redo
 # define variables
 region_array=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/vcfs.txt | tail -n1 )
 
-# filter for structure (minimum 45/49 individuals to keep a site)
+# filter for structure (minimum 49/49 individuals to keep a site)
 vcftools --vcf ${workdir}/04_vcf/${region_array} --keep ingroup.txt \
---max-missing 0.9 --mac 2 --max-alleles 2 --max-maf 0.49 --recode \
---recode-INFO-all --out ${workdir}/05_pca/structure45_${SLURM_ARRAY_TASK_ID}
+--max-missing 1.0 --thin 5000 --mac 2 --max-alleles 2 --max-maf 0.49 --recode \
+--recode-INFO-all --out ${workdir}/05_pca/structure49_${SLURM_ARRAY_TASK_ID}
 
