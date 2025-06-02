b1cc039b6e34febb7640485b46bcc453e0ac045c
Joe Manthey <jdmanthey@gmail.com>
2025-06-02 11:47:36 -0500

    Create 06_pca_admixture_redo.sh

---
+++ 06_pca_admixture_redo.sh
@@ -0,0 +1,53 @@
+interactive -p nocona -c 4 -m 8G
+
+source activate bcftools
+
+workdir=/lustre/scratch/jmanthey/greenbul_redo
+
+cd ${workdir}/05_pca
+
+
+##########################################
+##########################################
+### cat all individual scaffolds together
+##########################################
+##########################################
+
+grep "^#" structure49_34.recode.vcf > structure.vcf
+
+for i in $( ls structure49_*.recode.vcf ); do grep -v "^#" $i >> structure.vcf; done
+
+##########################################
+##########################################
+### remove all individual scaffold files
+##########################################
+##########################################
+
+rm *recode*
+
+##########################################
+##########################################
+### run plink for pca
+### and run ADMIXTURE
+##########################################
+##########################################
+
+# make chromosome map for this vcf
+grep -v "#" structure.vcf | cut -f 1 | uniq | awk '{print $0"\t"$0}' > chrom_map.txt
+
+#plink output format
+vcftools --vcf structure.vcf  --plink --chrom-map chrom_map.txt --out structure 
+
+# convert  with plink
+plink --file structure --recode12 --allow-extra-chr \
+--out structure_plink
+
+# run pca on dataset
+plink --file structure_plink --pca --allow-extra-chr \
+--out structure_plink_pca
+
+# convert with plink for admixture dataset
+plink --file structure --recode12 --allow-extra-chr --out admixture_plink
+
+# run admixture
+for K in 1 2 3 4 5; do admixture --cv admixture_plink.ped $K  | tee log_${K}.out; done
